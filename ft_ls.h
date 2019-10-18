acc7abfbf8a2a9d43843ef30d2ddd0528c952ee6
stasbovino <stasbovino@mail.ru>
2019-10-18 16:24:28 +0300

    upd

--- ft_ls.h
+++ ft_ls.h
@@ -1,6 +1,153 @@
 #ifndef FT_LS_H
 # define FT_LS_H
 
-#include "libft.h"
+# include "libft.h"
 
+/*
+** dir func
+*/
+
+# include <dirent.h>
+
+/*
+** getpwuid
+*/
+
+# include <sys/types.h>
+# include <pwd.h>
+# include <uuid/uuid.h>
+
+/*
+** getgrgid
+*/
+
+# include <grp.h>
+
+/*
+** listxattr getxattr
+*/
+
+# include <ses/xattr.h>
+
+/*
+** time ctime
+*/
+
+# include <time.h>
+
+
+typedef struct		s_flags
+{
+	char list_output; // -l
+	char recursive; // -R
+	char all_files; // -a
+	char reverse; // -r
+	char modif_date_sort; // -t
+
+/*
+** bonuses
+*/
+
+	char one_file_per_line_output; // -1
+	char colored_output; // -G
+	char access_date_sort; // -u
+	char creation_date_sort; // -U
+	char all_files_without_self; // -A
+	char hide_owner; // -g
+	char one_line_output; // -m
+	char dir_with_slash; // -p
+	char size_sort; // -S
+	char converted size; // -h
+}					t_flags;
+
+/*
+typedef struct		s_data
+{
+	int link;
+	int owner;
+	int group;
+	int fileSize;
+	int date_month;
+	int date_day;
+	int date_hour;
+	int date_minute;
+	int date_year;
+	int user_id;
+	int group_id;
+	int major;
+	int minor;
+}					t_data;
+
+
+typedef struct		s_date
+{
+	char *month;
+	char *day;
+	char *hour;
+	char *minute;
+	char *year;
+	unsigned long long mtv_sec;
+	unsigned long long mtv_nsec;
+	unsigned long long atv_sec;
+	unsigned long long atv_nsec;
+	unsigned long long ctv_sec;
+	unsigned long long ctv_nsec;
+	unsigned long long birthtv_sec;
+	unsigned long long birthtv_nsec;
+}					t_date;
+
+typedef struct		s_files
+{
+	char *modes;
+	long link;
+	char *owner;
+	unsigned int user_id;
+	char *group;
+	unsigned int group_id;
+	long size;
+	t_date date;
+	char *name;
+	int is_dir_info;
+	long major;
+	long minor;
+	int is_chr_or_blk;
+	int is_link;
+	char *linked_to;
+	int has_nonprintable_chars;
+	char *display_name;
+	int status;
+	struct s_files *next;
+	struct stat f; // Since I'm passing this I don't need to do stuffs like is_link or is_chr_or_blk
+}					t_files;
+
+typedef struct s_dirs {
+  char *name;
+  int status;
+  t_files *files;
+  t_files *self;
+  t_format format;
+  int       is_default;
+  int       is_unreadable;
+  int       total_blocks;
+  struct s_dirs *sub_dirs;
+  struct s_dirs *next;
+  int file_count;
+  int max_file_len;
+  t_date date;
+  int is_subdir;
+  char *display_name;
+  int has_chr_or_blk;
+  int has_valid_files;
+} t_dirs;
+
+typedef union u_etarget {
+  char flag;
+  char *file;
+} t_etarget;
+
+typedef union u_entries {
+  t_files *files;
+  char **file_names;
+} t_entries;e
+*/
 #endif
--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 
 int main(int argc, char **argv)
 {
+	t_dir *kek;
+
 	(void)argc;
 	argv = NULL;
 	ft_printf("hello ft_ls!\n");
